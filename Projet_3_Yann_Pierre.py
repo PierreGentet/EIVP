58266ac0192fcf3d0f3034f42231c97ec5cf05dc
INIZAN-29 <72786526+INIZAN-29@users.noreply.github.com>
2020-11-30 11:55:26 +0100

    Update Projet_3_Yann_Pierre.py

--- Projet_3_Yann_Pierre.py
+++ Projet_3_Yann_Pierre.py
@@ -218,6 +218,54 @@ def correlation():
     cor = covariance(var1,cap1,var2,cap2,s,e)/(math.sqrt(pvariance(var1,cap1,s,e))*math.sqrt(pvariance(var2,cap2,s,e)))
     return "le coefficient de correlation linéaire entre "+noms[var1]+" et "+noms[var2]+" est de "+str(cor)+"."
 
-def test():
-    return 4
+def anomalie():
+    var = ask_var()
+    cap = ask_cap()
+    s,e=ask_dates(cap)
+    ind,dates=interv(s,e,cap)
+    numdata1=numdata.copy()
+    anomalies=[] #création de liste vide pour y mettre les anomalies
+    moment_anomalies=[] #création de liste vide pour y mettre le moment des anomalies
+    i=1 #initialisation de i à 1 pour commencer à analyser la deuxième donnée, 
+    # on considère la première correcte
+    a=1 #le compteur a compte le nombre de valeur correcte
+    b=0
+    if var in [1,2,3,5,6]:
+        ecart_relatif=input("Ecart relatif entre 2 valeurs pour considérer que la valeur est anormale (en pourcentage):")
+        while i < int(len(numdata[var][cap])):
+            if (abs(numdata[var][cap][i] - numdata[var][cap][i-1])/(numdata[var][cap][i-1]+0.0001))> float(ecart_relatif): 
+                #on vérifie que l'écart relatif entre 2 valeurs consécutives est inférieur à l'écart relatif
+                print("anomalie trouvée à :", numdata[6][cap][i+b] ) #on affiche le moment des anomalies
+                anomalies.append(numdata[var][cap][i]) #on  affecte la valeurs de chaque anomalie 
+                #dans la liste "anomalies"
+                moment_anomalies.append(numdata[6][cap][i+b])
+                del numdata1[var][cap][i] #on supprime les valeurs des anomalies dans les données
+                i=i+1
+                b=b+1
+            else:
+                i=i+1
+                a=a+1
+        if a==len(numdata1[var][cap]):
+            print("aucune anomalie")
+        
+    else:
+        while i < int(len(numdata[var][cap])-1):
+            mx = max([numdata[var][cap][i-1],numdata[var][cap][i+1]])
+            mn = min([numdata[var][cap][i-1],numdata[var][cap][i+1]])
+            if  (mn-35)<=numdata[var][cap][i]<=(mx+35):
+                i=i+1
+                a=a+1
+            else :
+                print("anomalie trouvée à :", numdata[6][cap][i+b] ) #on affiche le moment des anomalies
+                anomalies.append(numdata[var][cap][i]) #on  affecte la valeurs de chaque anomalie 
+                #dans la liste "anomalies"
+                moment_anomalies.append(numdata[6][cap][i+b])
+                del numdata1[var][cap][i] #on supprime les valeurs des anomalies dans les données
+                i=i+1
+                b=b+1
+        if a==len(numdata1[var][cap]):
+            print("aucune anomalie")
+    print ("données correctes", numdata1[var][cap])
+    print ("données annormales", anomalies)
+    print ("moment où ces données sont annormales", moment_anomalies)
 
